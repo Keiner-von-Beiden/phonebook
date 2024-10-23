949068b9cc5a0645e2f24ba2d0d6d2de9a5c5021
Sergei Fedorov <s.n.feodorov@gmail.com>
2024-10-24 00:41:27 +0300

    Error handlers

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,26 @@ const Person = require('./models/person')
 app.use(cors())
 app.use(express.static('dist'))
 
-
-app.use(express.json())     // taking json-parser in usage
+app.use(express.json())     // taking json-parser in usage; should be among the very first middleware loaded (to get request.body available)
 app.use(morgan(':method method to :url, Status :status, Content lenght :res[content-length], Response time :response-time ms'))
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+//app.get('/', (request, response) => {
+//  response.send('<h1>Please go to <a href="api/persons">/api/persons</a> </h1>')
+//})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
@@ -19,22 +35,32 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
     .then(person => {
         if (person) {
           response.json(person)
         } else {
-          response.status(404).send({ warning: 'document not found'})
+          response.status(404).send({ error: 'document not found'})
         }
     })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
+app.get('/info', (request, response, next) => {
+  Person
+    .countDocuments({})
+    .then(count => {
+      response.send(`
+        <div>
+          <p>The phonebook has info for ${count} people</p>
+          <p>${new Date()}</p>
+        </div>
+      `)
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -53,11 +79,18 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person
     .findByIdAndDelete(request.params.id)
-    .then(response.status(204).end()) 
+    .then(result => {
+      // console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)      // should be just before the last middleware (no routes or middleware will be called after its response is sent -- except errorHandler)
+app.use(errorHandler)         // has to be the last loaded middleware
 
   
 const PORT = process.env.PORT
